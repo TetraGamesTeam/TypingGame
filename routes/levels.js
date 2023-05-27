d4c6c26b65419a8b1adc878b1116d96dbfb3bd87
TergaGamesTeam <mr.max.kh@gmail.com>
2023-05-28 01:56:28 +0700

    #7
    Починил систему оценки, добавил дисплей для следующей буквы, начал переносить стили

--- routes/levels.js
+++ routes/levels.js
@@ -13,33 +13,4 @@ router.get('/', (req, res) => {
   });
 });
 
-const levels = [
-  { answer: "привет", name: "Уровень 1" },
-  { answer: "мороженое", name: "Уровень 2" },
-  { answer: "крокодил", name: "Уровень 3" },
-];
-
-router.get('/:id', (req, res) => {
-  const id = req.params.id;
-  db.all('SELECT * FROM levels where id = ?', [id], (err, rows) => {
-    if (err) {
-      console.log(err);
-      res.status(500).send('Internal Server Error');
-      return;
-    }
-    if (rows.length === 0) {
-      res.status(404).send('Not Found');
-      return;
-    }
-    const row = rows[0];
-    res.render('individuallevel.nj', {
-      id: row.id,
-      comb1: row.combination1,
-      comb2: row.combination2,
-      comb3: row.combination3,
-      max_errors: row.max_errors
-    });
-  });
-});
-
 module.exports = router;
