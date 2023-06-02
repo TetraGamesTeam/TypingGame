5539f3033ea25dfb400e0247669a2d99201b3ae4
TergaGamesTeam <mr.max.kh@gmail.com>
2023-06-02 17:38:20 +0700

    #16

--- app.js
+++ app.js
@@ -2,46 +2,49 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const app = express();
 const nunjucks = require('nunjucks');
-
-
 const session = require('express-session');
 const crypto = require('crypto');
 const seckey = crypto.randomBytes(32).toString('hex');
 
-app.use(session({
-  secret: seckey,
-  resave: false,
-  saveUninitialized: false
-}));
+const app = express();
+const port = process.env.PORT || 8000;
 
-nunjucks.configure(__dirname + '/views', {
-  autoescape: true,
-  express: app
-});
-
-app.set('view engine', 'njk');
-
+// Настройка роутеров
 const indexRouter = require('./routes/index');
 const levelsRouter = require('./routes/levels');
-const regRouter = require('./routes/registration')
-const logRouter = require('./routes/login')
+const regRouter = require('./routes/registration');
+const logRouter = require('./routes/login');
 
+// Настройка нунчаков
+nunjucks.configure(path.join(__dirname, 'views'), {
+    autoescape: true,
+    express: app
+});
+
+// Настройка сессии с использованием секретного ключа
+app.use(session({
+    secret: seckey,
+    resave: false,
+    saveUninitialized: false
+}));
+
+// Использование middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Ковыряем роутеры
 app.use('/', indexRouter);
 app.use('/levels', levelsRouter);
 app.use('/registration', regRouter)
 app.use('/login', logRouter)
 
-const port = process.env.PORT || 8000;
+// Запуск сервера
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+    console.log(`Server listening on port ${port}`);
 });
 
 module.exports = app;
